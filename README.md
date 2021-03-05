763eddf99d204e8965d0fa8c36b1e495becac8c2
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-05 08:04:33 -0900

    Created arrays for destinations, restaurants, modes of transportation, and available forms of entertainment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DayTripGenerator_JavaScript
+
+Provides a list of destinations, restaurants, modes of transportation, and forms of entertainment 
+
+Generates a random list consists of of lists above ^^^ and produces a "day trip planner"
---
+++ daytripgenerator.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/// List of destinations
+
+let destinationArray = ["Seward", "Nome", "Juneau", "Anchorage"];
+let restaurantArray = ["Sullivan's Steakhouse", "Alaska Seafood Grill", "Husky Restaurant", "The Hangar On The Wharf"];
+let transportationArray = ["Car", "Plane", "Boat", "Bicycle"];
+let entertainmentArray = ["fishing", "hiking", "hunting", "snowboarding"];
+
+// Functions to randomize day trip
